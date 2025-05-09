b81c62289ce37b806059bd163c2d672e2719982b
Ieeeza <cesardermer1@gmail.com>
2025-05-09 12:29:28 -0300

    fix

--- angular-js/catalago-filmes/src/app/services/fetch-data.service.ts
+++ angular-js/catalago-filmes/src/app/services/fetch-data.service.ts
@@ -12,7 +12,7 @@ export class MovieService {
         const headers = new HttpHeaders({
             accept: "application/json",
             Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2ZkM2FlOGEyMzllMGY5MWI2NjAzZjQzMWVlMWFkMiIsIm5iZiI6MTc0NjgwMDQwMi40OTcsInN1YiI6IjY4MWUwZjEyMTVjZjhhMjEwYzBiMmZmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3wwgvU2mrloVGuOe365ndu24B4f9VvV3OU04jiVwjzA",
+                "Bearer
         });
 
         const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
